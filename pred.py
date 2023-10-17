1b95b2def07dae3fc47a1b4c8632d5a90af15a96
Hassan-Tamer <Hassantamerha@gmail.com>
2023-10-18 01:17:41 +0300

    final UI

--- pred.py
+++ pred.py
@@ -1,27 +1,92 @@
 from torchvggish import vggish, vggish_input
 import torch
-
-mod = torch.load("VGGish/best.pth")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-embedding_model = vggish()
-embedding_model.eval()
-
-pred_path = "/home/hassan/Documents/eme/Alghami/Dataset/genres_original/disco/disco.00000.wav"
-try:
-    example = vggish_input.wavfile_to_examples(pred_path)
-    embeddings = embedding_model.forward(example)
-    embeddings = embeddings.detach().numpy()
-    embeddings/=255.0
-    embeddings = embeddings.reshape(1,embeddings.shape[0]*embeddings.shape[1])
-    embeddings = torch.tensor(embeddings, dtype=torch.float32).to(device)
-except:
-    print("Error in extracting embedding")
-    exit(1)
+import torch.nn.functional as F
+import streamlit as st
+from scipy.io import wavfile
+import wave
+import soundfile as sf
+from pydub import AudioSegment
 
 
-out = mod(embeddings)
-pred = (torch.max(torch.exp(out), 1)[1]).data.cpu().numpy()
-classes = ("blues" , "classical" , "country" , "disco" , "hiphop" , "jazz" , "metal" , "pop" , "reggae" , "rock")
-print(classes[pred[0]])
+
+
+
+class NN_model(torch.nn.Module):
+        def _init_(self,input_size,hidden1_size,hidden2_size,hidden3_size,hidden4_size,hidden5_size,output_size):
+            super(NN_model,self)._init_()
+            self.fc1 = torch.nn.Linear(input_size, hidden1_size)
+            self.fc2 = torch.nn.Linear(hidden1_size, hidden2_size)
+            self.fc3 = torch.nn.Linear(hidden2_size, hidden3_size)
+            self.fc4 = torch.nn.Linear(hidden3_size, hidden4_size)
+            self.fc5 = torch.nn.Linear(hidden4_size, hidden5_size)
+            self.fc6 = torch.nn.Linear(hidden5_size, output_size)        
+
+        def forward(self,x):
+            out = F.leaky_relu(self.fc1(x))
+            out = F.leaky_relu(self.fc2(out))
+            out = F.leaky_relu(self.fc3(out))
+            out = F.leaky_relu(self.fc4(out))
+            out = F.leaky_relu(self.fc5(out))
+            
+            out = self.fc6(out)
+            return out
+        
+
+def predict(pred_path):
+    mod = torch.load("best.pth")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+    embedding_model = vggish()
+    embedding_model.eval()
+
+    try:
+        example = vggish_input.wavfile_to_examples(pred_path)
+        embeddings = embedding_model.forward(example)
+        embeddings = embeddings.detach().numpy()
+        embeddings/=255.0
+        embeddings = embeddings.reshape(1,embeddings.shape[0]*embeddings.shape[1])
+        embeddings = torch.tensor(embeddings, dtype=torch.float32).to(device)
+    except Exception as e:
+        print("Error in extracting embedding from audio file: ", e)
+        exit(1)
+
+
+    out = mod(embeddings)
+    pred = (torch.max(torch.exp(out), 1)[1]).data.cpu().numpy()
+    classes = ("blues" , "classical" , "country" , "disco" , "hiphop" , "jazz" , "metal" , "pop" , "reggae" , "rock")
+    print(classes[pred[0]])
+    return classes[pred[0]]
+
+st.title("Music Genre Classifier")
+
+# Create a file uploader
+uploaded_file = st.file_uploader("Upload a WAV File", type=["wav"])
+
+# Function to display information about the WAV file
+def display_wav_info(file):
+    if file is not None:
+        with st.spinner("Analyzing..."):
+            try:
+                wav = wave.open(file)
+                wavv = AudioSegment.from_wav(file)
+                wavv.export("file.wav", format="wav")
+                cls = predict("file.wav")
+                st.write('<span style="color: red; font-size: 24px;">The genre of the song is: ' + cls + '🎉' + '</span>', unsafe_allow_html=True)
+                num_channels = wav.getnchannels()
+                sample_width = wav.getsampwidth()
+                frame_rate = wav.getframerate()
+                num_frames = wav.getnframes()
+                duration = num_frames / float(frame_rate)
+
+                st.subheader("WAV File Information:")
+                st.write(f"Number of Channels: {num_channels}")
+                st.write(f"Sample Width (bytes): {sample_width}")
+                st.write(f"Frame Rate (samples per second): {frame_rate}")
+                st.write(f"Number of Frames: {num_frames}")
+                st.write(f"Duration (seconds): {duration:.2f} seconds")
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")
+
+# Display the WAV file information
+display_wav_info(uploaded_file)
